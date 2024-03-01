ade4c14fe23f20757882e0eeb3d1afa4af50ca1c
고경우 <dfdfg11@gmail.com>
2024-03-01 23:38:46 +0900

    start

---
+++ src/test/java/com/nyumtolic/nyumtolic/NyumtolicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nyumtolic.nyumtolic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NyumtolicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
