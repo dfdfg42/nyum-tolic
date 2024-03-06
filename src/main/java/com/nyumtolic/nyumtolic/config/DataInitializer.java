0efad622b31355757542e4c9820ee6addcebe647
고경우 <dfdfg11@gmail.com>
2024-03-06 18:38:39 +0900

    0306

--- src/main/java/com/nyumtolic/nyumtolic/config/DataInitializer.java
+++ src/main/java/com/nyumtolic/nyumtolic/config/DataInitializer.java
@@ -41,22 +41,43 @@ public class DataInitializer {
             Restaurant restaurant1 = new Restaurant();
             restaurant1.setName("북경");
             restaurant1.setCategories(Arrays.asList(chinese)); // 중식 카테고리 할당
+            restaurant1.setAddress("서울시 중구");
+            restaurant1.setPhoneNumber("02-123-4567");
+            restaurant1.setRating(4.5);
+            restaurant1.setTravelTime(15);
+            restaurant1.setPhoto("images/restaurant1.jpg"); // 이미지 경로 설정
             restaurantService.save(restaurant1);
 
             Restaurant restaurant2 = new Restaurant();
             restaurant2.setName("피자헛");
             restaurant2.setCategories(Arrays.asList(western)); // 양식 카테고리 할당
+            restaurant2.setAddress("서울시 강남구");
+            restaurant2.setPhoneNumber("02-765-4321");
+            restaurant2.setRating(4.0);
+            restaurant2.setTravelTime(20);
+            restaurant2.setPhoto("images/restaurant2.jpg"); // 이미지 경로 설정
             restaurantService.save(restaurant2);
 
             Restaurant restaurant3 = new Restaurant();
             restaurant3.setName("스위트홈");
             restaurant3.setCategories(Arrays.asList(dessert)); // 디저트 카테고리 할당
+            restaurant3.setAddress("서울시 어딘가");
+            restaurant3.setPhoneNumber("22-765-4321");
+            restaurant3.setRating(2.0);
+            restaurant3.setTravelTime(90);
+            restaurant3.setPhoto("images/restaurant3.jpg"); // 이미지 경로 설정
             restaurantService.save(restaurant3);
 
             Restaurant restaurant4 = new Restaurant();
             restaurant4.setName("한가람");
             restaurant4.setCategories(Arrays.asList(korean)); // 한식 카테고리 할당
+            restaurant4.setAddress("대구시 어딘가");
+            restaurant4.setPhoneNumber("11-235-4321");
+            restaurant4.setRating(1.0);
+            restaurant4.setTravelTime(30);
+            restaurant4.setPhoto("images/restaurant4.jpg"); // 이미지 경로 설정
             restaurantService.save(restaurant4);
+
         };
     }
 }
