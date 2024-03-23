eab96788c690167d615b38bdf9a1c5cea371ce5c
고경우 <dfdfg11@gmail.com>
2024-03-23 18:02:44 +0900

    0323 엑셀 수정

--- src/main/java/com/nyumtolic/nyumtolic/review/ReviewForm.java
+++ src/main/java/com/nyumtolic/nyumtolic/review/ReviewForm.java
@@ -2,6 +2,7 @@ package com.nyumtolic.nyumtolic.review;
 
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,5 +11,6 @@ import lombok.Setter;
 public class ReviewForm {
 
     @NotEmpty(message = "내용은 필수항목입니다")
+    @Size(min = 1, max = 500, message = "내용은 1자 이상 500자 이하로 작성해야 합니다")
     private String content;
 }
