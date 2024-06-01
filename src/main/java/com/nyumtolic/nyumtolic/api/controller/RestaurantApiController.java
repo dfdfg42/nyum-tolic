fb52233942d32a191e8e21132d20a2d82a9c0ee2
SolfE <devsolfe@gmail.com>
2024-06-01 23:53:01 +0900

    feat: update swagger docu

--- src/main/java/com/nyumtolic/nyumtolic/api/controller/RestaurantApiController.java
+++ src/main/java/com/nyumtolic/nyumtolic/api/controller/RestaurantApiController.java
@@ -4,6 +4,13 @@ package com.nyumtolic.nyumtolic.api.controller;
 import com.nyumtolic.nyumtolic.api.domain.PageResponse;
 import com.nyumtolic.nyumtolic.api.domain.RestaurantDTO;
 import com.nyumtolic.nyumtolic.service.RestaurantService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -15,11 +22,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
+@Tag(name = "음식점", description = "냠톨릭 음식점 데이터와 관련된 API입니다.")
 public class RestaurantApiController {
 
     private final RestaurantService restaurantService;
 
     @GetMapping("/restaurants")
+    @Operation(summary = "GET restaurant", description = "음식점 목록을 가져옵니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공",
+            content = {@Content(schema = @Schema(implementation = RestaurantDTO.class))})
+    })
     public PageResponse<RestaurantDTO> getCreateAPI(@PageableDefault(size = 5) Pageable pageable) {
         return restaurantService.getAllRestaurantsDTO(pageable);
     }
