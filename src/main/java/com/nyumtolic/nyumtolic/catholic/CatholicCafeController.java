aef630a84d24c103aef0f8c39d5881746436a883
고경우 <dfdfg11@gmail.com>
2025-03-18 23:41:55 +0900

    feat: 크롤링 컨트롤러, 서비스 , 타임리프 업데이트

--- src/main/java/com/nyumtolic/nyumtolic/catholic/CatholicCafeController.java
+++ src/main/java/com/nyumtolic/nyumtolic/catholic/CatholicCafeController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -20,7 +21,13 @@ public class CatholicCafeController {
         List<CatholicCafeTable> catholicCafeTable = catholicCafeTableRepository.findAll();
         for (CatholicCafeTable cafe : catholicCafeTable) {
             model.addAttribute(cafe.getName(), cafe.getLink());
-            model.addAttribute(cafe.getName() + "JPG", cafe.getS3Link());
+            // "cafeMensa"만 별도로 리스트로 변환하여 전달
+            if ("cafeMensa".equals(cafe.getName())) {
+                List<String> cafeMensaList = Arrays.asList(cafe.getS3Link().split(","));
+                model.addAttribute("cafeMensaList", cafeMensaList);
+            } else {
+                model.addAttribute(cafe.getName() + "JPG", cafe.getS3Link());
+            }
         }
         return "catholic/menu";
     }
--- src/main/java/com/nyumtolic/nyumtolic/catholic/CatholicCrawlerService.java
+++ src/main/java/com/nyumtolic/nyumtolic/catholic/CatholicCrawlerService.java
@@ -12,6 +12,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -37,7 +39,6 @@ public class CatholicCrawlerService {
     }
 
     public void updateData() {
-        //url 들 가져오기
         Map<String, String> data = CatholicCrawler.crawlCafeTable();
 
         for (String key : data.keySet()) {
@@ -45,30 +46,46 @@ public class CatholicCrawlerService {
 
             try {
                 ByteArrayResource pdf = CatholicCrawler.downloadPdf(url);
-                byte[] jpg = CatholicCrawler.convertPdfToJpg(pdf.getByteArray(), 0);
-                String s3Url = s3Service.uploadFile(new CustomMultipartFile(jpg, key, "image/jpeg"));
+                // 여러 페이지 업로드 URL을 담을 리스트
+                List<String> uploadedUrls = new ArrayList<>();
 
+                // cafeMensa 인 경우 2~4페이지(0-base index: 1, 2, 3)를 처리
+                if ("cafeMensa".equals(key)) {
+                    for (int pageIndex = 1; pageIndex <= 3; pageIndex++) {
+                        byte[] jpgBytes = CatholicCrawler.convertPdfToJpg(pdf.getByteArray(), pageIndex);
+                        // 파일명에 페이지 번호를 추가하여 S3에 업로드
+                        String fileName = String.format("%s-page%d.jpg", key, pageIndex + 1);
+                        String s3Url = s3Service.uploadFile(new CustomMultipartFile(jpgBytes, fileName, "image/jpeg"));
+                        uploadedUrls.add(s3Url);
+                    }
+                } else {
+                    // 다른 카페는 첫 페이지만 처리
+                    byte[] jpgBytes = CatholicCrawler.convertPdfToJpg(pdf.getByteArray(), 0);
+                    uploadedUrls.add(s3Service.uploadFile(new CustomMultipartFile(jpgBytes, key, "image/jpeg")));
+                }
+
+                // 업로드된 URL들을 comma로 구분해서 DB에 저장
+                String finalS3Link = String.join(",", uploadedUrls);
                 Optional<CatholicCafeTable> cafe = catholicCafeTableRepository.findByName(key);
                 CatholicCafeTable updatedCafe = cafe.orElseGet(() -> CatholicCafeTable.builder()
                         .name(key)
                         .link(url)
-                        .s3Link(s3Url)
                         .build());
-
+                updatedCafe.setS3Link(finalS3Link);
                 catholicCafeTableRepository.save(updatedCafe);
-                logger.info(key + "학식정보 업데이트 완료");
+                logger.info(key + " 학식정보 업데이트 완료 (" + uploadedUrls.size() + " page(s))");
 
             } catch (Exception e) {
                 Optional<CatholicCafeTable> cafe = catholicCafeTableRepository.findByName(key);
                 CatholicCafeTable updatedCafe = cafe.orElseGet(() -> CatholicCafeTable.builder()
                         .name(key)
                         .link(url)
-                        .s3Link("")
                         .build());
-
+                updatedCafe.setS3Link("");
                 catholicCafeTableRepository.save(updatedCafe);
-                logger.warn(key + "학식정보 업데이트 실패 " + url + "이 유효하지 않습니다.");
+                logger.warn(key + " 학식정보 업데이트 실패 " + url + " 이 유효하지 않습니다.");
             }
         }
     }
+
 }
